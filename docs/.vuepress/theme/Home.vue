f12f77d72adee08270e40999928353bb67977cde
colynn.liu <colynn.liu@gmail.com>
2021-12-14 20:20:26 +0800

    update: atomci 1.2.0 video link

--- docs/.vuepress/theme/Home.vue
+++ docs/.vuepress/theme/Home.vue
@@ -21,7 +21,7 @@
 
     <!-- <iframe src="http://player.bilibili.com/player.html?aid=547966151&cid=316359860&page=1" scrolling="no" width="900px" height="600px" border="0" frameborder="no" framespacing="0" allowfullscreen="true"> </iframe> -->
 
-    <iframe src="//player.bilibili.com/player.html?aid=547966151&cid=407287108&page=1" scrolling="no" width="800px" height="600px" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
+    <iframe src="//player.bilibili.com/player.html?aid=293904844&cid=407287108&page=1" scrolling="no" width="800px" height="600px" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
 
 
     <Content custom/>
--- docs/guide/README.md
+++ docs/guide/README.md
@@ -4,4 +4,6 @@ pageClass: getting-started
 
 # 介绍
 
-AtomCI 致力于让中小企业快速落地Kubernetes，以应用为中心，使用
+AtomCI 致力于让中小企业快速落地Kubernetes，以应用为中心
+
+<iframe src="//player.bilibili.com/player.html?aid=293904844&cid=413510859&page=1" scrolling="no" width="800px" height="600px" border="0" frameborder="no" framespacing="0" allowfullscreen="true"></iframe>
